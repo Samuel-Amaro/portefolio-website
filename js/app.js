212593113726b820365ea628e678693a6439a1b0
Samuel Amaro <samuelamaro96746313@gmail.com>
2022-07-12 11:45:41 -0300

    section about estilizada e menu navbar mobile pronto

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
+import {showMenu, hiddenMenu} from "./script.js";
+
 let btnMenu = document.querySelector(".button-menu");
+let menu = document.querySelector("nav");
 
-/*TODO: usar ponteiro de eventos para compatibilidade*/
-btnMenu.addEventListener("pointerclick", (event) => {
-
+//button mouse press
+btnMenu.addEventListener("pointerdown", (event) => {
+    if(menu.classList.contains("main-container--hide"))
+        showMenu();
+    else
+        hiddenMenu();
 });

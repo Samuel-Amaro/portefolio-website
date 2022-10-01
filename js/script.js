a45f44dddacbf65be74b695492a98b3dfebf735f
Samuel Amaro <samuelamaro96746313@gmail.com>
2022-10-01 10:52:53 -0300

    new project add

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
+"use strict";
+
 export function hiddenMenu() {
-    let menu = document.querySelector(".main-container__nav-bar-content");
-    menu.classList.remove("main-container__nav-bar-content");
-    menu.classList.add("main-container--hide");
+  let menu = document.querySelector(".main-container__nav-bar-content");
+  menu.classList.remove("main-container__nav-bar-content");
+  menu.classList.add("main-container--hide");
 }
 
 export function showMenu() {
-    let menu = document.querySelector(".main-container--hide");
-    menu.classList.remove("main-container--hide");
-    menu.classList.add("main-container__nav-bar-content");
+  let menu = document.querySelector(".main-container--hide");
+  menu.classList.remove("main-container--hide");
+  menu.classList.add("main-container__nav-bar-content");
 }
